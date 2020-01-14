2cb35e00228a182dfef5cb660fbf0bd99e620bd6
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2020-01-14 18:02:30 +0530

    Update Gatelogin.sql

--- Gatelogin.sql
+++ Gatelogin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 06, 2020 at 02:27 PM
+-- Generation Time: Jan 14, 2020 at 01:31 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -43,7 +43,10 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`id`, `qid`, `answer`, `timerq`, `diff`, `name`, `email`) VALUES
-(7, 31, ' Graduate Aptitude Test Engineering', 1, 'Low', 'Vaibhav Shukla', 'vibhu154@gmail.com');
+(7, 31, ' Graduate Aptitude Test Engineering', 1, 'Low', 'Vaibhav Shukla', 'vibhu154@gmail.com'),
+(8, 32, 'ONE OF IITS OR IISc CONDUCT GATE EXAM EVERY YEAR', 2, 'Low', 'Vaibhav Shukla', 'vaibhav321@gmail.com'),
+(9, 33, 'iits', 1, 'Low', 'Vaibhav Shukla', 'vibhu154@gmail.com'),
+(10, 32, 'Take a look on about section', 1, 'Low', 'Vimlesh Kumar', 'vim@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -64,7 +67,8 @@ CREATE TABLE `questionaddition` (
 
 INSERT INTO `questionaddition` (`id`, `que`, `name`, `email`) VALUES
 (31, 'What is the full form of GATE?', 'vibhu mishra', 'mishra@gmail.com'),
-(32, 'Who conduct GATE exam?', 'Vaibhav Shukla', 'vibhu154@gmail.com');
+(32, 'Who conduct GATE exam?', 'Vaibhav Shukla', 'vibhu154@gmail.com'),
+(33, 'Who conduct Gate exam?', 'Vaibhav Shukla', 'vaibhav321@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -87,9 +91,11 @@ CREATE TABLE `signin` (
 --
 
 INSERT INTO `signin` (`id`, `username`, `password`, `name`, `email`, `mobile`, `security`) VALUES
-(7, 'vaibhav154', 'shukla', 'Vaibhav Shukla', 'vibhu154@gmail.com', 9876543210, 'what is your favorite book?'),
-(8, 'vibhu', 'shukla', 'vibhu shukla', 'vibhu@gmail.com', 9658741230, 'where do you live?'),
-(9, 'vibhushukla', 'shukla154', 'vibhu mishra', 'mishra@gmail.com', 9586487562, 'which is your favorite book?');
+(7, 'vaibhav154', '123654', 'vimlesh', 'vibhu154@gmail.com', 9876543210, 'what is your favorite book?'),
+(8, 'vibhu', '123654', 'Vaibhav Shukla', 'vibhu@gmail.com', 9685741425, 'where do you live?'),
+(9, 'vibhushukla', 'shukla154', 'vibhu mishra', 'mishra@gmail.com', 9586487562, 'which is your favorite book?'),
+(10, 'vaibhav321', '654321', 'Vaibhav Shukla', 'vaibhav321@gmail.com', 9856741230, 'what is my favorite book?'),
+(11, 'vimlesh1107', '1107200', 'Vimlesh Kumar', 'vim@gmail.com', 88130714131, 'what is my hight?');
 
 --
 -- Indexes for dumped tables
@@ -121,19 +127,19 @@ ALTER TABLE `signin`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `questionaddition`
 --
 ALTER TABLE `questionaddition`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `signin`
 --
 ALTER TABLE `signin`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
